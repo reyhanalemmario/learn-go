d694eda6cb027135f07e241f027d87fee00065f3
Reyhan Alemmario <reyhanalemmario1@gmail.com>
2021-02-12 20:08:25 +0700

    first-episode

---
+++ tutorial.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Cruel World!")
+}
+
+// go run [filename.go] to automatically compile and run the program
+// go build [filename.go] to only compile the program
