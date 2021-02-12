55ad8dd5f0b9d47d45dadf3680179b48dde4fe39
Reyhan Alemmario <reyhanalemmario1@gmail.com>
2021-02-13 00:15:15 +0700

    Part 2

--- tutorial.go
+++ tutorial.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Cruel World!")
+	var name string = "Hello"
+
+	// var name_of_the_variable type
+	var name2 string
+	name2 = "Another Hello"
+	name2 = "Change Hello" //Overwrite
+
+	var number uint = 260
+
+	fmt.Println("Hello Cruel World!", name, name2, number)
 }
 
 // go run [filename.go] to automatically compile and run the program
